e1dee3f93a353d34d5f14328d43beb0f7ca6fc29
MAY <cm0850068@otc.edu>
2018-08-29 09:40:56 -0500

    Thrid Commit (Gotta Practice that JavaScript)

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
+//here is a comment
+/*multi line
+Cullen May
+
+
 console.log("Hello JavaScript");
+
+// Calue or Data types
+console.log("Hello from JavaScript!");
+console.log("Let's do some math!")
+console.log(4+7);
+console.log(12/0);
+console.log("Goodbye bitch!");
+
+let a= 3.14159265;
+console.log(a);
+
+
+const temp1= 34.5;
+const temp2=44.5;
+const temp3=66.87;
+console.log(temp1,temp2,temp3);
+temp3=0;
+
+
+let c = 0;
+c+=1;
+c++;
+console.log(c);
+
+
+num1= 0;
+{
+    num1=1;
+    const num2=0;
+    //console.log(num1);
+}
+
+console.log(num1);
+
+const country ="France";
+console.log(`I live in ${country}`);
+const x = 3;
+const y = 7;
+console.log(`${x} + ${y} = ${x+y}`);
+
+
+console.log(4+5);
+console.log("4 + 5");
+console.log("4" + "5");
+console.log(4 + "5");
+
+
+let num=0;
+let str="0";
+
+console.log(num===num);
+console.log(str===str);
+console.log(num===str);
+console.log(num==str);
+*/
+
+const x ="abc";
+switch (x) {
+    case "abc":
+    console.log("x=abc");
+    //break;
+    case "def":
+    console.log("x=def");
+    //breaK;
+    default:
+    console.log("otherwise");
+}
